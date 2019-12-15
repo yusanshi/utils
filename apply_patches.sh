e82d8301cca638ab240604935fcf5319a6f0a0c0
yusanshi <yulei2018@gmail.com>
2019-12-15 10:53:02 +0800

    Add manual mode and help message

--- apply_patches.sh
+++ apply_patches.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
-# Apply patches for all submodule
+# Apply patches for all submodules
 
+# How it works:
 # For submodule /path/to/foo/bar,
 # execute `cd /path/to/foo/bar && git apply ../bar.batch`
+
+if [ "$1" = "-h" -o "$1" = "--help" ]; then
+    echo "Apply patches for all submodules."
+    echo ""
+    echo "For submodule /path/to/foo/bar, patch file for it must be located"
+    echo "in /path/to/foo/bar.patch (generate_patches.sh would do this)."
+    exit 0
+fi
 
 wd=$(pwd)
 for i in $(git submodule foreach --quiet 'echo $path'); do
--- generate_patches.sh
+++ generate_patches.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
-# Generate patches for all submodule
+# Generate patches for all submodules
 
+# How it works:
 # For submodule /path/to/foo/bar,
 # execute `cd /path/to/foo/bar && git add . && git diff --cached > ../bar.patch`
 # So patch for it would be located in /path/to/foo/bar.patch
+
+
+if [ "$1" = "-h" -o "$1" = "--help" ]; then
+    echo "Generate patches for all submodules."
+    echo ""
+    echo "Usage: generate_patches.sh [options]"
+    echo ""
+    echo "Options:"
+    echo -e "--help | -h\t\tShow this message, then exit."
+    echo -e "--manual | -m\t\tManually choose whether add binary files for every submodule. "
+    echo -e "\t\t\tDefault mode is auto mode, which automatically process all submodules,"
+    echo -e "\t\t\tignoring binary files them."
+    exit 0
+fi
 
 wd=$(pwd)
 for i in $(git submodule foreach --quiet 'echo $path'); do
     cd "$wd"/"$i"
     git add .
-    diff=$(git diff --cached)
+    if [ "$1" = "-m" -o "$1" = "--manual" ]; then
+        read -p "Add binary files for submodule $i? [yN]" choice
+        case $choice in
+        [Yy]*) diff=$(git diff --cached --binary) ;;
+        *) diff=$(git diff --cached) ;;
+        esac
+    else
+        diff=$(git diff --cached)
+    fi
     if [[ -n $diff ]]; then
         patch=${PWD##*/}.patch
         echo "$diff" >../"$patch"
